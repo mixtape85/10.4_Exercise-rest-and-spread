c8052071412924e6affa055dc5c3432b3516d448
Mandy Woodbury <85962983+mixtape85@users.noreply.github.com>
2021-09-10 22:16:43 -0400

    add ES2015 functions for doubleAndReturnArgs, removeRandom, extend, and addKeyVal

--- app.js
+++ app.js
@@ -39,35 +39,34 @@ Write a function called doubleAndReturnArgs which accepts an array and a variabl
 doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
 doubleAndReturnArgs([2],10,4) // [2, 20, 8]
 */
-
+const doubleAndReturnArgs = (arr, ...args) => [...arr, (...args).map(num => num *2)];
 
 /* 
 Slice and Dice!
 For this section, write the following functions using rest, spread and refactor these functions to be arrow functions!
-
 Make sure that you are always returning a new array or object and not modifying the existing inputs.
 */
 
 /** remove a random element in the items array
 and return a new array without that item. */
 
-function removeRandom(items) {
-
+const removeRandom = (items) => {
+  let i = Math.floor(Math.random()*items.length);
+  return [...items.slice(0,i), ...items.slice(i+1)];
 }
 
 /** Return a new array with every item in array1 and array2. */
 
-function extend(array1, array2) {
-
-}
+const extend = (array1, array2) => [...array1, ...array2];
 
 /** Return a new object with all the keys and values
 from obj and a new key/value pair */
 
-function addKeyVal(obj, key, val) {
-
+const addKeyVal = (obj, key, val) => {
+  let obj2 = {...obj};
+  obj2[key] = val;
+  return obj2;
 }
-
 
 /** Return a new object with a key removed. */
 
